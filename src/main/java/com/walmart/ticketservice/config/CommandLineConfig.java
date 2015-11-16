d1006c7672027386d80f6b9f7ee498c379fc89cd
Steve <wsapiens@hotmail.com>
2015-11-15 20:43:24 -0600

    add missing file in commit

---
+++ src/main/java/com/walmart/ticketservice/config/CommandLineConfig.java
@@ -0,0 +1,5 @@
+package com.walmart.ticketservice.config;
+
+public class CommandLineConfig {
+
+}
---
+++ src/main/java/com/walmart/ticketservice/consts/Consts.java
@@ -0,0 +1,5 @@
+package com.walmart.ticketservice.consts;
+
+public class Consts {
+	public static final String MODE_TEST = "test";
+}
